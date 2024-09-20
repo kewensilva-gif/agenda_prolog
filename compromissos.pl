e64e014ed7678eff50fdda14c744f1beb2028103
Talysson Vasconcelos <talyssonpestinha.tp@gmail.com>
2024-09-20 20:15:12 -0300

    novas atualizacoes

--- compromissos.pl
+++ compromissos.pl
@@ -1,6 +1,5 @@
 :- dynamic compromisso/3.
 
-compromisso(data(22, 22, 2024), hora(11, 11), 'jantar com lula').
 compromisso(data(15, 15, 2024), hora(15, 16), 'mayliy ed2').
 compromisso(data(30, 10, 2024), hora(15, 15), 'kelly paz').
 compromisso(data(2, 8, 2000), hora(8, 2), 'niver da pombada').
